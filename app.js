8f26341b2abb5fabb24d46aa60d492e4dde4b88e
Ahmed Elkheshin <ahmed.elkheshin@gmail.com>
2016-09-12 10:12:53 -0400

    Added front end functionality in angualr

--- app.js
+++ app.js
@@ -9,44 +9,76 @@ angular.module("rap-duel", ['ui.router'])
       url: '/home',
       templateUrl: '/home.html',
       controller: 'MainCtrl'
+    })
+
+    .state('users', {
+      url: 'users/{id}',
+      templateUrl: '/users.html',
+      controller: 'UsersCtrl'
     });
 
     $urlRouterProvider.otherwise('home');
   }])
-  .factory('battles', [function(){
+  .factory('users', [function(){
     var o = {
-      battles: []
+      users: []
     };
     return o;
   }])
-  .controller("MainCtrl", [
-    "$scope",
-    'battles',
-    function($scope, battles){
-      $scope.battles = battles.battles;
-      //hardcoded data
-      $scope.battles = [
-        {title: 'battle 1', user_name1: "Eminem", user_name2: "Benzino", audio1_Url: "#", audio2_Url:"#"},
-        {title: 'battle 2', user_name1: "KRS-ONE", user_name2: "MC Shan" , audio1_Url: "#", audio2_Url:"#"},
-        {title: 'battle 3', user_name1: "2pac", user_name2: "Notorious B.I.G.", audio1_Url: "#", audio2_Url:"#"}
-      ]
-      //form to create a new battle
+  .controller("UsersCtrl", [
+    '$scope',
+    '$stateParams',
+    'users',
+    function($scope, $stateParams, users){
+      $scope.user = users.users[$stateParams.id];
       $scope.addBattle = function(){
-        //prevents a user from submitting a blank battle
-        if(!$scope.title || $scope.title === '') {return; }
-        if(!$scope.user_name1 || $scope.user_name1 === '') {return; }
-        if(!$scope.audio1_Url || $scope.audio1_Url === '') {return; }
-        $scope.battles.push({
+        if($scope.title === '') {return;}
+        $scope.user.battles.push({
           title: $scope.title,
-          user_name1: $scope.user_name1,
+          challenger_name: 'user',
           audio1_Url: $scope.audio1_Url,
-          user_name2: $scope.user_name2,
-          audio2_Url:$scope.audio2_Url
+          audio2_Url: $scope.audio2_Url
         });
         $scope.title = '';
-        $scope.user_name1 = '';
+        $scope.challenger_name = '';
         $scope.audio1_Url = '';
-        $scope.user_name2 = '';
         $scope.audio2_Url = '';
       };
-    }]);
+    }])
+    .controller("MainCtrl", [
+      "$scope",
+      'users',
+      function($scope, users){
+        $scope.users = users.users;
+        //hardcoded data
+        $scope.users = [
+          {rap_name: 'Mos Def', image_url: "http://s1.evcdn.com/images/edpborder500/I0-001/004/337/040-0.jpeg_/mos-def-40.jpeg", city: "Brooklyn, NY", description: "Fun loving hip hoper!"},
+          {rap_name: 'Black Thought', image_url: "http://static.djbooth.net/pics-features/black-thought-hly-sht-harvard.jpg", city:"Philadelphia, PA", description: "I love me battle rap!"},
+          {rap_name: 'Eminem', image_url: "http://ceoworld.biz/ceo/wp-content/uploads/2009/09/eminem-pic.jpg", city:"Detroit, MI", description: "Everyone's favorite villian!"}
+        ]
+        $scope.users.push({
+          rap_name: $scope.rap_name,
+          image_url: $scope.image_url,
+          city: $scope.city,
+          description: $scope.description,
+          battles: [
+            {title: "Thrilla in Manilla", challenger_name: "Nas", audio1_Url: "#", audio2_Url: "#"},
+            {title: "Rumble in the Jungle", challenger_name: "DMX", audio1_Url: "#", audio2_Url: "#"}
+          ]
+        });
+        //form to create a new battle
+        $scope.addUser = function(){
+          //prevents a user from submitting a blank battle
+          if(!$scope.rap_name || $scope.rap_name === '') {return; }
+          $scope.users.push({
+            rap_name: $scope.rap_name,
+            image_url: $scope.image_url,
+            city: $scope.city,
+            description: $scope.description
+          });
+          $scope.rap_name = '';
+          $scope.image_url = '';
+          $scope.city = '';
+          $scope.description = '';
+        };
+      }]);
