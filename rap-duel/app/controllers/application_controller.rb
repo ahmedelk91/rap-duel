a75853fbbf84dd4ad76d4b4fcefc046905a1f2fd
Ahmed Elkheshin <ahmed.elkheshin@gmail.com>
2016-09-13 11:11:52 -0400

    added bower_components

--- rap-duel/app/controllers/application_controller.rb
+++ rap-duel/app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  root to: 'application#angular'
+
+  def angular
+    render 'layout/application'
+  end
 end
